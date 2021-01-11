194db7594c0444a3b36068cc5de5538439615abd
Ethan Champion <35370825+ethanchampion@users.noreply.github.com>
2021-01-11 01:35:23 +0000

    Interim Report Progress
    A commit to show the progress made, as of the interim report (10th January 2021)

---
+++ Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DimGray
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(-1, -1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(174, 141)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connections"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DimGray
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(-1, 140)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(174, 141)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Map"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DimGray
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(-1, 281)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(174, 141)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Programs"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.DimGray
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(-1, 421)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(174, 141)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Firewall Rules"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Location = New System.Drawing.Point(172, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(811, 563)
+        Me.Panel1.TabIndex = 4
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(333, 35)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 2
+        Me.Button6.Text = "Kill Process"
+        Me.Button6.UseVisualStyleBackColor = True
+        Me.Button6.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(7, 6)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(320, 544)
+        Me.ListBox1.TabIndex = 1
+        Me.ListBox1.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(333, 6)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "Refresh"
+        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(984, 561)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "FireSpy"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button4 As Button
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Private Sub UpdateProcessList()
+        ListBox1.Items.Clear()
+        Dim p As Process
+        For Each p In Process.GetProcesses()
+            ListBox1.Items.Add(p.ProcessName & " - " & p.Id.ToString())
+        Next
+        ListBox1.Sorted = True
+        ListBox1.Text = "Processes running: " & ListBox1.Items.Count.ToString()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.BackColor = Color.LightGray
+        Button1.ForeColor = Color.Black
+        Button2.BackColor = Color.DimGray
+        Button2.ForeColor = Color.White
+        Button3.BackColor = Color.DimGray
+        Button3.ForeColor = Color.White
+        Button4.BackColor = Color.DimGray
+        Button4.ForeColor = Color.White
+        ListBox1.Visible = True
+        Button5.Visible = True
+        Button6.Visible = True
+        UpdateProcessList()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button1.BackColor = Color.DimGray
+        Button1.ForeColor = Color.White
+        Button2.BackColor = Color.LightGray
+        Button2.ForeColor = Color.Black
+        Button3.BackColor = Color.DimGray
+        Button3.ForeColor = Color.White
+        Button4.BackColor = Color.DimGray
+        Button4.ForeColor = Color.White
+        ListBox1.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button1.BackColor = Color.DimGray
+        Button1.ForeColor = Color.White
+        Button2.BackColor = Color.DimGray
+        Button2.ForeColor = Color.White
+        Button3.BackColor = Color.LightGray
+        Button3.ForeColor = Color.Black
+        Button4.BackColor = Color.DimGray
+        Button4.ForeColor = Color.White
+        ListBox1.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button1.BackColor = Color.DimGray
+        Button1.ForeColor = Color.White
+        Button2.BackColor = Color.DimGray
+        Button2.ForeColor = Color.White
+        Button3.BackColor = Color.DimGray
+        Button3.ForeColor = Color.White
+        Button4.BackColor = Color.LightGray
+        Button4.ForeColor = Color.Black
+        ListBox1.Visible = False
+        Button5.Visible = False
+        Button6.Visible = False
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        UpdateProcessList()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If ListBox1.SelectedItems.Count < 1 Then
+            MessageBox.Show("Click on a process name to select it.", "No Process Selected")
+            Return
+        End If
+        Dim p As Process
+        For Each p In Process.GetProcesses()
+            Dim arr() As String = ListBox1.SelectedItem.ToString().Split("-")
+            Dim stringProcess As String = arr(0).Trim()
+            Dim itemId As Integer = Convert.ToInt32(arr(1).Trim())
+            If p.ProcessName = stringProcess And p.Id = itemId Then
+                p.Kill()
+            End If
+        Next
+        UpdateProcessList()
+    End Sub
+End Class
